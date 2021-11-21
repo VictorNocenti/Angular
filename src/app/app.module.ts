5b33f5aeaed99a28e593bc2db31d99f6bd9f1ec0
Akshit <Akshit.aashrawat@gmail.com>
2021-11-21 17:01:12 +0530

    036 Understanding Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HttpClient, HttpClientJsonpModule, HttpClientModule, HttpClientXsrfModule} from "@angular/common/http";
+import {ServerComponent} from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
+import {WarningAlertComponent} from "./warning-alert/warning-alert.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ServerComponent,
+        ServersComponent,
+        WarningAlertComponent
+    ],
+    imports: [
+        HttpClientModule,
+        HttpClientJsonpModule,
+        HttpClientXsrfModule,
+        BrowserModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
